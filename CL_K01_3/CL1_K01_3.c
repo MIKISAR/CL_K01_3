6ea82840b4ab10be11005ccbbbb4c48e222c91b4
TORII KAZUKI <k_t_paniponi@yahoo.co.jp>
2013-01-15 09:42:25 +0900

    応用プログラミングA の課題1
    初課題！

---
+++ CL_K01_3/CL1_K01_3.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+/* ì¬Ò:1T2(18) ’¹ˆäˆê÷ */
+/* ‰Û‘è–¼:‰Û‘è01_3 */
+
+void main(void){
+	double radius,x;
+	radius=5.0;
+	x=3.141593;
+
+	printf("”¼Œa%f‚Ì‰~ü‚Í%f‚Å‚·B\n",radius,radius*2*x);
+	printf("”¼Œa%f‚Ì–ÊÏ‚Í%f‚Å‚·B",radius,radius*radius*x);
+}
